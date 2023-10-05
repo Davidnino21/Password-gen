7b6958ef516b1a64a5acd0822e80c4be168b0e95
Davidnino21 <davidnino_21@yahoo.com>
2023-10-05 15:08:12 -0700

    comments added

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# Password Gen
+# Password Generator
 
 ## Description
-
-## User story
+Created is a password generator that creates a unique password with a length from 8 to 128 and gives the option to include uppercase, lowercase, numeric, and special characters.
+Creating a secure password will help user to keep their personal information safe.
--- script.js
+++ script.js
@@ -7,18 +7,18 @@ var specialChars = "!@#$&*?_-"
 var chars = "";
 
 //the generatePassword fuction is defined. this function is called when generate password button is clicked.
-  function generatePassword() {
-    var passwordLength = prompt("How many characters would you like your password to contain?");
-    if (passwordLength.length > 0) {
-      passwordLength = parseInt(passwordLength)
-    } else {
-      passwordLength = 0;
-    }
-    if (passwordLength < 8 || passwordLength > 128) {
-      alert("Password length cant be less than 8 or more than 128")
-      passwordLength = prompt("How many characters would you like your password to contain?");
-    }
-  
+function generatePassword() {
+  var passwordLength = prompt("How many characters would you like your password to contain?");
+  if (passwordLength.length > 0) {
+    passwordLength = parseInt(passwordLength)
+  } else {
+    passwordLength = 0;
+  }
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Password length cant be less than 8 or more than 128")
+    passwordLength = prompt("How many characters would you like your password to contain?");
+  }
+
   var specialCharacters = confirm("Click OK to confirm including special characters.");
   var numericCharacters = confirm("Click OK to confirm including numeric characters.");
   var lowercase = confirm("click OK to confirm including lowercase characters.");
@@ -44,21 +44,21 @@ var chars = "";
     chars = chars + upperCaseChars;
     console.log('after', chars);
   }
-
+  //this variable stores the generated password and the loop is used to generate the password length
   var password = "";
   for (var i = 0; i < passwordLength; i++) {
     var randomIndex = Math.floor(Math.random() * chars.length);
     password += chars[randomIndex];
   }
-
+  //this function is responsible for displaying the password on the page
   writePassword(password);
 }
-
+//this selects an HTML element and updates the text input field
 function writePassword(password) {
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
-
+//this variable is used to select generate password. the event listener listen for the click
 var generateBtn = document.querySelector("#generate")
 generateBtn.addEventListener("click", generatePassword)
 
